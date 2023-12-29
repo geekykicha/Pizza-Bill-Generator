c058c565a4b5d44c1c85cc840d5b357a7f65ed31
Krishna <96795053+geekykicha@users.noreply.github.com>
2023-12-29 15:09:33 +0530

    Update Main.java

--- PizzaBillGenerate/src/com/PizzaHut/Pizza/Main.java
+++ PizzaBillGenerate/src/com/PizzaHut/Pizza/Main.java
@@ -4,11 +4,16 @@ package com.PizzaHut.Pizza;
 public class Main {
 
 	public static void main(String[] args) {
+		
+		// for base pizza 
+		
 //		Pizza basePizza = new Pizza(true);
 //		basePizza.addExtraCheese();
 //		basePizza.addExtraToppings();
 //		basePizza.takeAway();
 //		basePizza.getBill();
+		
+		// for delux pizza
 		
 		DeluxPizza dp = new DeluxPizza(true);
 		dp.addExtraCheese();
